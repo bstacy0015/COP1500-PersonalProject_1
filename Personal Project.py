9453b2414aea64c9f9d7bce3fbca1ec338c9198b
Benton <54415436+bstacy0015@users.noreply.github.com>
2019-12-12 21:50:17 -0500

    Uploaded files.
    ExerciseData.csv is the data imported from Samsung Health that I'll be analyzing. The Test.py file ensures that all operations are good to go, and Personal Project.py is the main file that'll do the data analysis (not done).

---
+++ Personal Project.py
@@ -0,0 +1,42 @@
+"""
+Source 1 (used in dropping specific rows):
+https://chrisalbon.com/python/data_wrangling/pandas_dropping_column_and_rows/
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+path = "C:\\Users\\bento\\Documents\\Personal Project\\ExerciseData.csv"
+df = pd.read_csv(path)
+
+
+# Pre-processing Data:
+
+# Remove NaN's which could cloud the data:
+df.dropna(subset=["MeanSpeed"], axis=0, inplace=True)  # Removes data entries
+# which don't have a MeanSpeed entry.
+df.dropna(subset=["MaxSpeed"], axis=0, inplace=True)  # Removes data entries
+# which don't have a MaxSpeed entry.
+df.dropna(subset=["Distance"], axis=0, inplace=True)  # Removes data entries
+# which don't have a Distance entry.
+df.dropna(subset=["Calories"], axis=0, inplace=True)  # Removes data entries
+# which don't have a Calories entry.
+
+# Format all relevant data so proper analysis can be made:
+df["MeanSpeed"] = df["MeanSpeed"].astype("float")  # Ensures that all
+# MeanSpeed entries are float values.
+df["MaxSpeed"] = df["MaxSpeed"].astype("float")  # Ensures that all
+# MaxSpeed entries are float values.
+df["Distance"] = df["Distance"].astype("float")  # Ensures that all
+# Distance entries are float values.
+df["Calories"] = df["Calories"].astype("float")  # Ensures that all
+# Calories entries are float values.
+df["Start"] /= 1000  # Change UNIX Epoch time (with ms) to UNIX Epoch time
+df["End"] /= 1000  # Change UNIX Epoch time (with ms) to UNIX Epoch time
+
+# Only include walking data so I can do an analysis on only walking data:
+df = df[df.ExerciseType == 1001]  # In my imported data, walking is
+# ExerciseType "1001"
+
+print(df.head(50))
---
+++ Test.py
@@ -0,0 +1,10 @@
+"""
+Source:
+http://purcellconsult.com/getting-the-hang-of-pycharm-massive-tutorial-lab/
+_
+"""
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [5, 10, 24.82, 72.39, 102]
+plt.plot(x, y)  # Plots the x and y data on the x and y-axis, respectively
+plt.show()  # Prints a plot
