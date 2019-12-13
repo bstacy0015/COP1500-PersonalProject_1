91919b95f5b3ffa596f005b1aa85e32cdfee9361
Benton <54415436+bstacy0015@users.noreply.github.com>
2019-12-12 22:29:35 -0500

    Convert to American units

--- Personal Project.py
+++ Personal Project.py
@@ -7,7 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-path = "C:\\Users\\bento\\Documents\\Personal Project\\ExerciseData.csv"
+path = "C:\\Users\\bento\\Documents\\COP1500-PersonalProject_1\\ExerciseData" \
+       ".csv"
 df = pd.read_csv(path)
 
 
@@ -26,12 +27,21 @@ df.dropna(subset=["Calories"], axis=0, inplace=True)  # Removes data entries
 # Format all relevant data so proper analysis can be made:
 df["MeanSpeed"] = df["MeanSpeed"].astype("float")  # Ensures that all
 # MeanSpeed entries are float values.
+df["MeanSpeed"] *= 2.23694  # Converts MeanSpeed from meters per second to
+# miles per hour
+
 df["MaxSpeed"] = df["MaxSpeed"].astype("float")  # Ensures that all
 # MaxSpeed entries are float values.
+df["MaxSpeed"] *= 2.23694  # Converts MaxSpeed from meters per second to
+# miles per hour
+
 df["Distance"] = df["Distance"].astype("float")  # Ensures that all
 # Distance entries are float values.
+df["Distance"] /= 1609.344  # Converts Distance from meters to miles
+
 df["Calories"] = df["Calories"].astype("float")  # Ensures that all
 # Calories entries are float values.
+
 df["Start"] /= 1000  # Change UNIX Epoch time (with ms) to UNIX Epoch time
 df["End"] /= 1000  # Change UNIX Epoch time (with ms) to UNIX Epoch time
 
